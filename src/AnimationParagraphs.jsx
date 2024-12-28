6b4135a0d50c599589393871b6c1738cf8ba35ea
4hmedgit <ahmed.infinitybits@gmail.com>
2024-12-28 22:49:59 +0500

    second

--- src/AnimationParagraphs.jsx
+++ src/AnimationParagraphs.jsx
@@ -13,13 +13,24 @@ const AnimationParagraphs = () => {
         className="mb-4"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 0.5, duration: 1 }}
+        transition={{ delay: 1, duration: 1 }}
       >
         Every day with you feels like a dream come true.
         <div className="flex justify-center flex-wrap">
           <span>
             I love you more than words could ever express. You are the light of
-            my life, and I cherish every moment with you.
+            my life, and I cherish every moment with you. I know we disagree on
+            alot of things and are almost polar opposite of each other but we
+            got through everything together and i think that made our bond so
+            strong. Thankyou for not giving up on me and changing yourself for
+            me and putting in all that effort for us, if it wasnt for you we
+            would not be together right now and I would have regreted that till
+            the very end. I am the luckiest person in the world to get such a
+            gorgeous girl so easily. I hope the coming year brings you all the
+            hapiness you can ask for and we be together through thick and thin
+            and I hope the love u have for me never ends even if we are 90 years
+            old you will still cherish me like u do now. Aur bs shadi kr lo ab
+            jaldi se mujhsy.
           </span>
           <img src={heart} width={32} />
         </div>
--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,36 +1,11 @@
 import React, { useState, useEffect } from "react";
 import kiss from "./assets/kissing.gif";
 
-import img1 from "./assets/11.jpg";
-import img2 from "./assets/12.jpg";
-import img3 from "./assets/13.jpg";
-import img4 from "./assets/14.jpg";
-import img6 from "./assets/16.jpg";
-import img22 from "./assets/22.jpg";
-import img33 from "./assets/33.jpg";
-import img44 from "./assets/44.jpg";
-import img55 from "./assets/55.jpg";
-import img66 from "./assets/66.jpg";
-import img77 from "./assets/77.jpg";
-import img88 from "./assets/88.jpg";
-import img99 from "./assets/99.jpg";
+// Import all images dynamically using import.meta.glob
+const images = Object.values(
+  import.meta.glob("./assets/*.jpg", { eager: true, import: "default" })
+);
 
-const images = [
-  img1,
-  img2,
-  img3,
-  img4,
-  // img5,
-  img6,
-  img22,
-  img33,
-  img44,
-  img55,
-  img66,
-  img77,
-  img88,
-  img99,
-];
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -43,16 +18,15 @@ const Carousel = () => {
   }, []);
 
   return (
-    <div className="relative py-8">
+    <div className="relative pt-4">
       <div className="font-semibold text-xl text-center">
         To all the memories we made
       </div>
-      <div className="font-semibold text-xl justify-center py-4 flex items-center ">
-        <div className="  text-center ">along the way</div>
+      <div className="font-semibold text-xl justify-center  flex items-center">
+        <div className="text-center">along the way</div>
         <img src={kiss} width={32} alt="Kiss emoji" className="ml-2" />
       </div>
       <div className="overflow-hidden">
-        {" "}
         <div
           className="flex transition-transform duration-1000 ease-in-out"
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
@@ -62,7 +36,7 @@ const Carousel = () => {
               <img
                 src={image}
                 alt={`Image ${index + 1}`}
-                className="w-full h-full object-contain"
+                className="w-full h-[600px] py-8 object-contain"
               />
             </div>
           ))}
